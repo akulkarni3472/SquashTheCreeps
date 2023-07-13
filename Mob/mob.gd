23f03f2dad4e097000f12a50a0380e31b380ae3d
Kulkarni <Amrut.Kulkarni@xylem.com>
2023-07-13 19:13:34 -0400

    Added animation

---
+++ Mob/mob.gd
@@ -0,0 +1,22 @@
+extends CharacterBody3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@export var min_speed = 10
+@export var max_speed = 20
+
+func _physics_process(_delta):
+	move_and_slide()
+
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	
+	var random_speed = randi_range(min_speed, max_speed)
+	
+	velocity = Vector3.FORWARD * random_speed
+	
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
---
+++ Player/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+
+@export var speed = 10
+@export var jump_vel = 7
+@export var lerp_val = 10
+
+@onready var pivot = $Pivot
+@onready var animation_player = $AnimationPlayer
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta):
+    fall(delta)
+    move(delta)
+    jump()
+
+func fall(delta):
+    if not is_on_floor():
+        velocity.y -= gravity * delta
+
+func move(delta):
+    var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+    var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+    if direction:
+        velocity.x = direction.x * speed
+        velocity.z = direction.z * speed
+    else:
+        velocity.x = move_toward(velocity.x, 0, speed)
+        velocity.z = move_toward(velocity.z, 0, speed)
+        
+    if direction != Vector3.ZERO:
+        direction = direction.normalized()
+        animation_player.speed_scale = 4
+        pivot.rotation.y = lerp_angle(pivot.rotation.y, atan2(-velocity.x, -velocity.z), delta * lerp_val)
+    else:
+        animation_player.speed_scale = 1
+    move_and_slide()
+
+func jump():
+    if Input.is_action_just_pressed("jump") and is_on_floor():
+        velocity.y = jump_vel
